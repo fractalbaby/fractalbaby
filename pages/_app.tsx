2b935c10eee8f33b00261cb0eea92888e5efc4c6
fractalbaby <thefractalbaby@gmail.com>
2022-01-09 19:49:37 +0100

    Animate background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,12 @@ import { useEffect, useState } from 'react'
 
 const theme = {
   ...defaultTheme,
+	animations: {
+    spin: 'x-spin 1s linear infinite',
+    ping: 'x-ping 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+    pulse: 'x-pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+    bounce: 'x-bounce 1s infinite',
+  },
 }
 
 const trackList = [
